50ae67af9925d948cfb2c650e7ce767a9b4ca8a6
Devesh Jain <76024137+devesh2511@users.noreply.github.com>
2023-10-11 11:35:41 +0530

    upd1

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CardComponent } from './card/card.component';
+import { FooterComponent } from './footer/footer.component';
+import { MessageComponent } from './message/message.component';
+import { PlansComponent } from './plans/plans.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    FooterComponent,
+    MessageComponent,
+    PlansComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
